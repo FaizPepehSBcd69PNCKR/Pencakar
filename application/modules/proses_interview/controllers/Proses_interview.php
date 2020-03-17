66b3f5a4d9f28ededf5fe93b7f867bb34244df7c
Mohammad-Riski-R <riskisempu157@gmail.com>
2020-03-17 20:08:04 +0700

    opopop

---
+++ application/modules/proses_interview/controllers/Proses_interview.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Proses_interview extends MX_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		// model
+		 $this->load->model('m_proses_interview');
+		
+	}
+
+	
+	// index
+	function index()
+	{
+		$data = array(
+			'akunhmvc' 		=> "Proses_interview",
+			'akunhmvcview' 	=> "V_proses_interview",
+			'tampil'        => $this->m_proses_interview->tampil(),
+		);
+		echo Modules::run('template/tampilCore_back_end', $data);
+	}
+}
