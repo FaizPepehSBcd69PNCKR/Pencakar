dd76dab8580afed08474de34cca8a1cac0a7e264
tolesendidwi <62248135+tolesendidwi@users.noreply.github.com>
2020-03-18 17:52:55 +0700

    rete
    helen

--- application/modules/p_profil_saya/views/V_p_profil_saya.php
+++ application/modules/p_profil_saya/views/V_p_profil_saya.php
@@ -18,12 +18,12 @@
 				<div class="col-md-4">
           <div class="form-group">
           <label style="font-size: 18px; margin-left: 7%; margin-top: 10%;">Foto</label>
-					<div class="form-group" style="margin-left: 7%; margin-top: -4%;">
+					<div class="form-group" style="margin-left: 7%; margin-top: 5%;">
                 <img  src="<?= base_url(); ?>assets/cakar/img/<?= $aku->logo_perusahaan ?>" width="180px" height="180px">
 				    </div>
 				</div>
       </div>
-				<div class="col-md-8 add_top_30" style="margin-left: -0%; margin-top: 5%;">
+				<div class="col-md-8 add_top_30" style="margin-left: -10%; margin-top: 5%;">
 					<div class="row">
 						<div class="col-md-6">
 							<div class="form-group">
@@ -60,48 +60,6 @@
 				</div>
 			</div>
 		</div>
-		<!-- /box_general-->
-		<div class="row">
-			<div class="col-md-6">
-				<div class="box_general padding_bottom">
-					<div class="header_box version_2">
-						<h2><i class="fa fa-lock"></i>Ganti Kata sandi</h2>
-					</div>
-          <br>
-					<div class="form-group">
-						<input class="form-control" type="password" placeholder="Masukkan kata sandi yang lama">
-					</div>
-          <br>
-					<div class="form-group">
-						<input class="form-control" type="password" placeholder="Masukkan kata sandi baru">
-					</div>
-          <br>
-					<div class="form-group">
-						<input class="form-control" type="password" placeholder="Konfirmasi kata sandi baru">
-					</div>
-				</div>
-			</div>
-			<div class="col-md-6">
-				<div class="box_general padding_bottom">
-					<div class="header_box version_2">
-						<h2><i class="fa fa-envelope"></i>Ganti email</h2>
-					</div>
-					<div class="form-group">
-						<br>
-						<input class="form-control" name="old_email" id="old_email" type="email" placeholder="Masukkan email lama">
-					</div>
-					<div class="form-group">
-						<br>
-						<input class="form-control" name="new_email" id="new_email" type="email" placeholder="Masukkan email baru">
-					</div>
-					<div class="form-group">
-						<br>
-						<input class="form-control" name="confirm_new_email" id="confirm_new_email" type="email" placeholder="Konfirmasi email baru">
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- /row-->
 		<p style="text-align: center;"><a href=""class="btn_1 medium" class="nav-link" data-toggle="modal" data-target="#edit_profil">Edit</a></p>
 	  </div>
 	</div>
@@ -126,32 +84,32 @@
         <input type="hidden" name="id_rl_perusahaan" value="<?php echo $aku->id_rl_perusahaan ?>">
                 <div class="col-md-4">
                     <div class="form-group"><br>
-                    <label>Foto</label><br><br>
+                    <label style="margin-top: 0px">Foto</label><br><br>
                          <input type="file" name="logo_perusahaan" class="form-control" id="userfile" onchange="tampilkanPreview(this,'preview_3x4')">
                     </div>
                 </div>
 
                 <label for="" class="control-label"></label>
-            <img src="<?= base_url(); ?>assets/cakar/img/<?= $aku->logo_perusahaan ?>" id="preview_3x4" width="150" height="150" />
+            <img style="margin-top: 123px; margin-left: -250px; " src="<?= base_url(); ?>assets/cakar/img/<?= $aku->logo_perusahaan ?>"  id="preview_3x4" width="237" height="220"  />
 
                 <div class="col-md-8 add_top_30">
                     <div class="row">
                         <div class="col-md-6">
-                            <div class="form-group">
+                            <div style="margin-left: 40px; margin-top:10px" class="form-group">
                                 <label>Nama perusahaan</label>
-                                <input type="text" name="nama_perusahaan"  class="form-control" value="<?php echo $aku->nama_perusahaan?>">
+                                <input type="text" style="width: 236px" name="nama_perusahaan"  class="form-control" value="<?php echo $aku->nama_perusahaan?>">
                             </div>
                         </div>
                     <div class="col-md-6">
-              <div class="form-group">
-                <label>Telepon perusahaan</label>
+              <div style="margin-left: 20px" class="form-group">
+                <label style="margin-top: 10px"> perusahaan</label>
                 <input type="text" value="<?php echo $aku->telepon_perusahaan ?>" name="telepon_perusahaan" class="form-control">
               </div>
             </div>
                     </div>
                     <!-- /row-->
                     <div class="row">
-                        <div class="col-md-6">
+                        <div style="margin-left: 40px" class="col-md-6">
                             <div class="form-group">
                                 <label>Email perusahaan</label>
                                 <input type="text" value="<?php echo $aku->email_perusahaan ?>" name="email_perusahaan"  class="form-control">
@@ -160,7 +118,7 @@
                     </div>
                     <div class="row">
 						<div class="col-md-12">
-							<div class="form-group">
+							<div style="margin-left: 40px" class="form-group">
 								<label>Alamat lengkap perusahaan</label>
 								<input type="text" style="height:100px;" value="<?php echo $aku->alamatlengkap_perusahaan ?>" name="alamatlengkap_perusahaan" class="form-control">
 							</div>
@@ -182,7 +140,6 @@
   </div>
   </form>
 <?php } ?>
-
 
 <script>
 function tampilkanPreview(userfile,idpreview)
