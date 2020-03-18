6cc9b4b4c85a9bb7e8407a6019a15f25af070d8c
Mohammad-Riski-R <riskisempu157@gmail.com>
2020-03-18 14:30:11 +0700

    k

---
+++ application/modules/profil_saya/controllers/profil_saya.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class profil_saya extends MX_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		// model
+		 $this->load->model('m_profil_saya');
+		 
+	}
+
+	
+	// index
+	function index()
+	{
+		$data = array(
+			'akunhmvc'		 	=> "profil_saya",
+			'akunhmvcview' 	    => "V_profil_saya",
+			'tampil'        	=> $this->m_profil_saya->tampil(),
+		);
+		echo Modules::run('template/tampilCore_back_end2', $data);
+	}
+
+	function edit()
+	{
+		$this->m_profil_saya->edit();
+		
+	}
+	
+}
+?>
