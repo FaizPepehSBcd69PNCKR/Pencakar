05ccab0c1e581509266356e252cd083f497009e2
FaizPepehSBcd69PNCKR <62247484+FaizPepehSBcd69PNCKR@users.noreply.github.com>
2020-03-28 15:54:46 +0700

    nyar

---
+++ application/libraries/fungsi.php
@@ -0,0 +1,20 @@
+<?php 
+/**
+ * 
+ */
+class Fungsi extends AnotherClass
+{
+
+	protected $ci;
+
+	function _construct(){
+		$this->ci =& get_instance();
+	}
+
+	function user_login()
+	$this->ci->load->model();
+	
+}
+
+
+ ?>
